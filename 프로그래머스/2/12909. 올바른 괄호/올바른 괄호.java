7b01371a334b5e3c1179893d9c6a9b78be40392b
JeongJun Min <41982054+m3k0813@users.noreply.github.com>
2025-04-07 18:26:14 +0900

    [level 2] Title: 올바른 괄호, Time: 26.75 ms, Memory: 55.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        Stack<Character> st = new Stack<>();
+        
+        for(char c : s.toCharArray()){
+            
+            if (c == '(') {
+                
+                st.push(c);
+            } else {
+                
+                if(st.isEmpty()){
+                    
+                    return false;
+                }
+                st.pop();
+            }
+        }
+        return st.isEmpty();
+    }
+}
